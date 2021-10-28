ebf13673750020ff4cc4f63b94ef4f5e77c4ad79
abdelrahman Amer <abdelrahman.amer1999@gmail.com>
2021-10-28 23:35:34 +0200

    nearly finish

--- lib/main.dart
+++ lib/main.dart
@@ -14,88 +14,37 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            //mainAxisSize: MainAxisSize.min,
-            //verticalDirection: VerticalDirection.up,
-            //mainAxisAlignment: MainAxisAlignment.end/center,
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenlyBetween,
-            //crossAxisAlignment: CrossAxisAlignment.end,
-            //crossAxisAlignment: CrossAxisAlignment,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 70.0,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('assets/cv2pic.jpeg'),
-              ),
-              Text(
-                'Abdelrahman Amer',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+              //mainAxisSize: MainAxisSize.min,
+              //verticalDirection: VerticalDirection.up,
+              //mainAxisAlignment: MainAxisAlignment.end/center,
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenlyBetween,
+              //crossAxisAlignment: CrossAxisAlignment.end,
+              //crossAxisAlignment: CrossAxisAlignment,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 70.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('assets/cv2pic.jpeg'),
                 ),
-              ),
-              Text('Software Engineer',
+                Text(
+                  'Abdelrahman Amer',
                   style: TextStyle(
-                    fontFamily: 'source sans proReg',
-                    color: Colors.teal.shade100,
-                    fontSize: 20.0,
-                    letterSpacing: 2.5,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
                     fontWeight: FontWeight.bold,
-                  )),
-              Card(
-                
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding: EdgeInsets.all(25.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        '+20 1272198897',
-                        style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'source sans proReg',
-                            fontSize: 20.0),
-                      ),
-                    ],
                   ),
                 ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Padding(
-                  padding:EdgeInsets.all(20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.mail,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Abdelrahman.amer1999@gmail.com',
-                        style: TextStyle(
-                            color: Colors.teal.shade900,
-                            fontFamily: 'source sans proReg',
-                            fontSize: 17.0),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+                Text('Software Engineer',
+                    style: TextStyle(
+                      fontFamily: 'source sans proReg',
+                      color: Colors.teal.shade100,
+                      fontSize: 20.0,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    )),
+
+              ]),
         ),
       ),
     );
